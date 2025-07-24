hmatthewsraj-aug25/mattbigbluebutton

commit ebf4e27570fb888149380ad6b8793a69c89e413a

fix(layou): apply meeting layout only when pushed
Only assigns the meeting layout to the selected layout when the layout
is currently being pushed, otherwise stick with the local one.

bigbluebutton-html5/imports/ui/components/layout/push-layout/pushLayoutEngine.jsx
@@ -116,7 +116,7 @@ const PushLayoutEngine = (props) => {
     const enforcedLayout = LAYOUT_TYPE[enforceLayoutResult] || null;
 
     Settings.layout.selectedLayout = enforcedLayout || changeLayout || defaultLayout
-      || meetingLayout || currentLayout;
+      || (pushLayoutMeeting ? (meetingLayout || currentLayout) : (currentLayout));
 
     if (isMobile()) {
       currentLayout = currentLayout === 'custom' ? 'smart' : currentLayout;

commit 8917e3d376283e501d4096880fbef2a45f738bf6

fix(layout): apply meetingLayout and hide_presentation_on_join properly
This commit removes an incorrect condition introduced in ebf4e27570fb,
which prevented `meetingLayout` from being applied when `pushLayout` was
false.

It also fixes an issue where `hide_presentation_on_join=true` was not
respected. The push layout engine wrongly replicated the default server
value (presentation open), because the current user data from the GraphQL
subscription was undefined. As a result, the user was misidentified as
not being the presenter on the first render.

bigbluebutton-html5/imports/ui/components/layout/push-layout/pushLayoutEngine.jsx
@@ -116,7 +116,7 @@ const PushLayoutEngine = (props) => {
     const enforcedLayout = LAYOUT_TYPE[enforceLayoutResult] || null;
 
     Settings.layout.selectedLayout = enforcedLayout || changeLayout || defaultLayout
-      || (pushLayoutMeeting ? (meetingLayout || currentLayout) : (currentLayout));
+      || meetingLayout || currentLayout;
 
     if (isMobile()) {
       currentLayout = currentLayout === 'custom' ? 'smart' : currentLayout;
@@ -422,8 +422,6 @@ const PushLayoutEngineContainer = (props) => {
     isModerator: user.isModerator,
     presenter: user.presenter,
   }));
-  const isModerator = currentUserData?.isModerator;
-  const isPresenter = currentUserData?.presenter;
 
   const presentationVideoRate = calculatePresentationVideoRate(cameraDockOutput);
 
@@ -435,6 +433,10 @@ const PushLayoutEngineContainer = (props) => {
     presentationInput,
     layoutSettings,
   );
+
+  if (!currentUserData || currentUserData === null) return null;
+  const isModerator = currentUserData?.isModerator;
+  const isPresenter = currentUserData?.presenter;
 
   const validateEnforceLayout = (currUser) => {
     const layoutTypes = Object.keys(LAYOUT_TYPE);