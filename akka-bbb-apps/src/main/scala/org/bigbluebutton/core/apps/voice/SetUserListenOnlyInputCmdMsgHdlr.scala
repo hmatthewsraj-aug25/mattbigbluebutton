f28cd30f6c52bf4e4c60bace75720407ec599099
Ramón Souza <contato@ramonsouza.com>
2025-08-07 15:04:16 -0300

    remove unused code

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/voice/SetUserListenOnlyInputCmdMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/voice/SetUserListenOnlyInputCmdMsgHdlr.scala
@@ -1,11 +1,9 @@
 package org.bigbluebutton.core.apps.voice
 
 import org.bigbluebutton.common2.msgs.SetUserListenOnlyInputCmdMsg
-import org.bigbluebutton.core.apps.{ PermissionCheck, RightsManagementTrait }
-import org.bigbluebutton.core.apps.voice.VoiceApp
-import org.bigbluebutton.core.models.{ Roles, Users2x, VoiceUsers }
+import org.bigbluebutton.core.apps.{ RightsManagementTrait }
+import org.bigbluebutton.core.models.{ VoiceUsers }
 import org.bigbluebutton.core.running.{ BaseMeetingActor, LiveMeeting, OutMsgRouter }
-import org.bigbluebutton.core2.MeetingStatus2x
 
 trait SetUserListenOnlyInputCmdMsgHdlr extends RightsManagementTrait {
   this: BaseMeetingActor =>
@@ -15,16 +13,11 @@ trait SetUserListenOnlyInputCmdMsgHdlr extends RightsManagementTrait {
 
   def handleSetUserListenOnlyInputCmdMsg(msg: SetUserListenOnlyInputCmdMsg) {
     val meetingId = liveMeeting.props.meetingProp.intId
-    val voiceConf = liveMeeting.props.voiceProp.voiceConf
 
     log.info("Received listen only input device change for user request. meetingId=" + meetingId + " userId="
       + msg.header.userId + " listenOnlyInputDevice=" + msg.body.listenOnlyInputDevice)
 
     for {
-      requester <- Users2x.findWithIntId(
-        liveMeeting.users2x,
-        msg.header.userId
-      )
       u <- VoiceUsers.findWithIntId(
         liveMeeting.voiceUsers,
         msg.header.userId
