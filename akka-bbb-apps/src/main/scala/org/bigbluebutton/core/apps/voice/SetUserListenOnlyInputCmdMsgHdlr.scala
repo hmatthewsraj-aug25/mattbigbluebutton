c327f225c1413ac115a58dc01b399ee29c968817
Ramón Souza <contato@ramonsouza.com>
2025-08-07 14:50:55 -0300

    remove unnecessary check

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/voice/SetUserListenOnlyInputCmdMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/voice/SetUserListenOnlyInputCmdMsgHdlr.scala
@@ -14,41 +14,31 @@ trait SetUserListenOnlyInputCmdMsgHdlr extends RightsManagementTrait {
   val outGW: OutMsgRouter
 
   def handleSetUserListenOnlyInputCmdMsg(msg: SetUserListenOnlyInputCmdMsg) {
-    if (msg.body.userId != msg.header.userId && (permissionFailed(
-      PermissionCheck.MOD_LEVEL,
-      PermissionCheck.VIEWER_LEVEL, liveMeeting.users2x, msg.header.userId
-    ))) {
-      val meetingId = liveMeeting.props.meetingProp.intId
-      val reason = "No permission to set listen only input device for user."
-      PermissionCheck.ejectUserForFailedPermission(meetingId, msg.header.userId, reason, outGW, liveMeeting)
-    } else {
-      val meetingId = liveMeeting.props.meetingProp.intId
-      val voiceConf = liveMeeting.props.voiceProp.voiceConf
+    val meetingId = liveMeeting.props.meetingProp.intId
+    val voiceConf = liveMeeting.props.voiceProp.voiceConf
 
-      log.info("Received listen only input device change for user request. meetingId=" + meetingId + " userId="
-        + msg.body.userId + " listenOnlyInputDevice=" + msg.body.listenOnlyInputDevice)
+    log.info("Received listen only input device change for user request. meetingId=" + meetingId + " userId="
+      + msg.body.userId + " listenOnlyInputDevice=" + msg.body.listenOnlyInputDevice)
 
-      for {
-        requester <- Users2x.findWithIntId(
-          liveMeeting.users2x,
-          msg.header.userId
+    for {
+      requester <- Users2x.findWithIntId(
+        liveMeeting.users2x,
+        msg.header.userId
+      )
+      u <- VoiceUsers.findWithIntId(
+        liveMeeting.voiceUsers,
+        msg.body.userId
+      )
+    } yield {
+      if (u.listenOnlyInputDevice != msg.body.listenOnlyInputDevice) {
+        log.info("Send set listen only input device user request. meetingId=" + meetingId + " userId=" + u.intId + " listenOnlyInputDevice=" + msg.body.listenOnlyInputDevice)
+        VoiceApp.setListenOnlyInputInVoiceConf(
+          liveMeeting,
+          outGW,
+          u.intId,
+          msg.body.listenOnlyInputDevice
         )
-        u <- VoiceUsers.findWithIntId(
-          liveMeeting.voiceUsers,
-          msg.body.userId
-        )
-      } yield {
-        if (u.listenOnlyInputDevice != msg.body.listenOnlyInputDevice) {
-          log.info("Send set listen only input device user request. meetingId=" + meetingId + " userId=" + u.intId + " listenOnlyInputDevice=" + msg.body.listenOnlyInputDevice)
-          VoiceApp.setListenOnlyInputInVoiceConf(
-            liveMeeting,
-            outGW,
-            u.intId,
-            msg.body.listenOnlyInputDevice
-          )
-        }
       }
     }
-
   }
 }
