62596bfcb72137f79360742c1c09901f09e049d3
Gabriel Porfirio <gabrielporfirio1994@gmail.com>
2025-08-07 09:30:32 -0300

    add @ci flag

--- bigbluebutton-tests/playwright/user/user.spec.js
+++ bigbluebutton-tests/playwright/user/user.spec.js
@@ -172,7 +172,7 @@ test.describe.parallel('User', { tag: ['@ci', '@flaky-3.1'] }, () => {
       });
     });
 
-    test.describe.parallel('Lock viewers', () => {
+    test.describe.parallel('Lock viewers', { tag: '@ci' }, () => {
       // https://docs.bigbluebutton.org/3.0/testing/release-testing/#webcam
       test('Lock Share webcam', async ({ browser, context, page }) => {
         const lockViewers = new LockViewers(browser, context);
