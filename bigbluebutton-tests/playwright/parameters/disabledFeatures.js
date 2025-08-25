307d5c77b143390361d655a573298e9a25bde1bd
Gabriel Luiz Porfirio <gabrielporfirio1994@gmail.com>
2025-08-25 16:26:17 -0300

    test: Update few parameters tests (#23531)
    * update few parameters tests
    
    * adding more checks to override default locale
    
    * updated selector messagesSidebarButton

--- bigbluebutton-tests/playwright/parameters/disabledFeatures.js
+++ bigbluebutton-tests/playwright/parameters/disabledFeatures.js
@@ -9,7 +9,7 @@ class DisabledFeatures extends MultiUsers {
   }
 
   async breakoutRooms() {
-    await this.modPage.waitAndClick(e.manageUsers);
+    await this.modPage.waitAndClick(e.breakoutRoomSidebarButton);
     await this.modPage.wasRemoved(e.createBreakoutRoomsButton, 'should not display the option to create breakout rooms on the manage users');
   }
 
